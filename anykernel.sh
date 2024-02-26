dfa366e7be8d83623eab7b75037288d54035690f
AshBorn <anishsingh0119@gmail.com>
2024-02-26 12:59:29 +0530

    Fix String

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=KernelSU_Builder_Nashc for Realme 8 (nashc) (version_of_auto_build_you_flash)
+kernel.string=Kernel Builder For Realme 8
 do.devicecheck=1
 do.modules=0
 do.systemless=1
